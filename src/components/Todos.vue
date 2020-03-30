3232f043397602bce03e174127b549685de491c9
Joshua <jjspscl@gmail.com>
2020-03-30 15:52:17 +0800

    v-bind w/ conditions

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="">
+    <div v-bind:key="todo.id" v-for="todo in todos" class="">
+      <TodoItem v-bind:todo="todo" />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"],
+}
+</script>
+
+<style lang="css" scoped>
+</style>
